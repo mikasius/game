0088486f923d38242eb37edf9d567c0141ba14d8
Sergey Metlenkin <sergei.metlenkin@mcc-tomsk.de>
2020-12-22 11:51:22 +0700

    WIP

--- src/Questions/index.tsx
+++ src/Questions/index.tsx
@@ -1,86 +1,105 @@
-import React from 'react';
+import React  from 'react';
+import { v4 as uuid } from 'uuid';
+import { Cell } from './Cell';
 import Styles from './index.module.scss';
+import { Row } from './Row';
 
 enum Categories {
-  Games,
-  MCC,
-  Sport,
-  Cinema,
+  Games = 'Games',
+  MCC = 'MCC',
+  Sport = 'Sport',
+  Cinema = 'Cinema',
 }
 type Question = {
+  id: string;
   price: string;
   question: string;
   answer: string;
 };
-type QuestionsType = {
+type Quiz = {
   [category: string]: Question[];
 };
 
-const questions: QuestionsType = {
+const quiz: Quiz = {
   [Categories.Games]: [
-    { price: '100', question: '1?', answer: '1!' },
-    { price: '200', question: '2?', answer: '2!' },
-    { price: '300', question: '3?', answer: '3!' },
-    { price: '400', question: '4?', answer: '4!' },
-    { price: '500', question: '5?', answer: '5!' },
+    { id: uuid(), price: '100', question: '1?', answer: '1!' },
+    { id: uuid(), price: '200', question: '2?', answer: '2!' },
+    { id: uuid(), price: '300', question: '3?', answer: '3!' },
+    { id: uuid(), price: '400', question: '4?', answer: '4!' },
+    { id: uuid(), price: '500', question: '5?', answer: '5!' },
   ],
   [Categories.MCC]: [
-    { price: '100', question: '1?', answer: '1!' },
-    { price: '200', question: '2?', answer: '2!' },
-    { price: '300', question: '3?', answer: '3!' },
-    { price: '400', question: '4?', answer: '4!' },
-    { price: '500', question: '5?', answer: '5!' },
+    { id: uuid(), price: '100', question: '1?', answer: '1!' },
+    { id: uuid(), price: '200', question: '2?', answer: '2!' },
+    { id: uuid(), price: '300', question: '3?', answer: '3!' },
+    { id: uuid(), price: '400', question: '4?', answer: '4!' },
+    { id: uuid(), price: '500', question: '5?', answer: '5!' },
   ],
   [Categories.Sport]: [
-    { price: '100', question: '1?', answer: '1!' },
-    { price: '200', question: '2?', answer: '2!' },
-    { price: '300', question: '3?', answer: '3!' },
-    { price: '400', question: '4?', answer: '4!' },
-    { price: '500', question: '5?', answer: '5!' },
+    { id: uuid(), price: '100', question: '1?', answer: '1!' },
+    { id: uuid(), price: '200', question: '2?', answer: '2!' },
+    { id: uuid(), price: '300', question: '3?', answer: '3!' },
+    { id: uuid(), price: '400', question: '4?', answer: '4!' },
+    { id: uuid(), price: '500', question: '5?', answer: '5!' },
   ],
   [Categories.Cinema]: [
-    { price: '100', question: '1?', answer: '1!' },
-    { price: '200', question: '2?', answer: '2!' },
-    { price: '300', question: '3?', answer: '3!' },
-    { price: '400', question: '4?', answer: '4!' },
-    { price: '500', question: '5?', answer: '5!' },
+    { id: uuid(), price: '100', question: '1?', answer: '1!' },
+    { id: uuid(), price: '200', question: '2?', answer: '2!' },
+    { id: uuid(), price: '300', question: '3?', answer: '3!' },
+    { id: uuid(), price: '400', question: '4?', answer: '4!' },
+    { id: uuid(), price: '500', question: '5?', answer: '5!' },
   ],
 };
 
 export const Questions = () => {
-  const questionsMapped = Object.values(questions);
+  const openQuestion = (id: string) => {
+    console.log(id);
+  };
 
   return (
     <main className={Styles.questions}>
-      {questionsMapped.map((category, index) => (
-        <div
-          style={{
-            border: 1,
-            borderColor: 'tomato',
-            borderStyle: 'solid',
-            marginTop: 16,
-            marginBottom: 16,
-          }}
-        >
-          <b
-            style={{
-              display: 'inline-block',
-              minWidth: 70,
-              paddingRight: 16,
-              textAlign: 'right',
-            }}
-          >
-            {Categories[index]}
-          </b>
-          {category.map((question) => (
-            <div style={{ display: 'inline-block' }}>
-              <div style={{ padding: 16 }}>{question.price}</div>
-              <div style={{ display: 'none' }}>{question.question}</div>
-              <div style={{ display: 'none' }}>{question.answer}</div>
-            </div>
+      {Object.values(quiz).map((questions, index) => (
+        <Row key={index}>
+          {questions.map((question, index) => (
+            <Cell key={index}/>
           ))}
-        </div>
+        </Row>
       ))}
+      {/*{questionsMappedKey.map((category) => (*/}
+      {/*  <div*/}
+      {/*    style={{*/}
+      {/*      border: 1,*/}
+      {/*      borderColor: 'tomato',*/}
+      {/*      borderStyle: 'solid',*/}
+      {/*      marginTop: 16,*/}
+      {/*      marginBottom: 16,*/}
+      {/*    }}*/}
+      {/*  >*/}
+      {/*    <b*/}
+      {/*      style={{*/}
+      {/*        display: 'inline-block',*/}
+      {/*        minWidth: 70,*/}
+      {/*        paddingRight: 16,*/}
+      {/*        textAlign: 'right',*/}
+      {/*      }}*/}
+      {/*    >*/}
+      {/*      {category}*/}
+      {/*    </b>*/}
+      {/*    {Object.values(questions).map((question) => (*/}
+      {/*      <div style={{ display: 'inline-block' }}>*/}
+      {/*        <div style={{ padding: 16 }}>*/}
+      {/*          <button onClick={() => openQuestion(3)} style={{}}>*/}
+      {/*            {question}*/}
+      {/*          </button>*/}
+      {/*        </div>*/}
+      {/*        <div style={{ display: 'none' }} datatype={'3'}>*/}
+      {/*          {question.question}*/}
+      {/*        </div>*/}
+      {/*        <div style={{ display: 'none' }}>{question.answer}</div>*/}
+      {/*      </div>*/}
+      {/*    ))}*/}
+      {/*  </div>*/}
+      {/*))}*/}
     </main>
   );
 };
